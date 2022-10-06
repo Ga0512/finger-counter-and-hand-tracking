10058b18268274de520b4de2559f0b38b95b18e1
Gabriel Cicotoste <gabrielmurilocicotoste6@gmail.com>
2022-10-06 12:10:10 -0300

    countfinger

---
+++ counterfinger.py
@@ -0,0 +1,58 @@
+import cv2
+from opencv import handmodulo as htm
+
+wCam, hCam = 640, 480
+
+cap = cv2.VideoCapture(0)
+cap.set(3, wCam)
+cap.set(4, hCam)
+
+'''folderPath = "FingerImages"
+myList = os.listdir(folderPath)
+print(myList)
+overlayList = []
+for imPath in myList:
+    image = cv2.imread(f'{folderPath}/{imPath}')
+    # print(f'{folderPath}/{imPath}')
+    overlayList.append(image)
+
+print(len(overlayList))'''
+
+detector = htm.handDetector(detectionConfidence=0.75)
+
+tipIds = [4, 8, 12, 16, 20]
+
+while True:
+    success, img = cap.read()
+    img = detector.findHands(img)
+    lmList = detector.findPosition(img, draw=False)
+    # print(lmList)
+
+    if len(lmList) != 0:
+        fingers = []
+
+        # polegar
+        if lmList[tipIds[0]][1] > lmList[tipIds[0] - 1][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+
+        # 4 dedos
+        for id in range(1, 5):
+            if lmList[tipIds[id]][2] < lmList[tipIds[id]-2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+
+        # print(fingers)
+        totalFingers = fingers.count(1)
+        print(totalFingers)
+
+        '''h, w, c = overlayList[totalFingers - 1].shape
+        img[0:h, 0:w] = overlayList[totalFingers - 2]'''
+
+        cv2.putText(img, str(totalFingers), (0, 125), cv2.FONT_HERSHEY_PLAIN,
+                    10, (11, 11, 11), 10)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
